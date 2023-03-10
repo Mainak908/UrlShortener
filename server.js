4b52aa56cbb43ce87cf1910254b76bbb2795c2f0
Piyush Kumar ✔ <110088028+Piyush7980@users.noreply.github.com>
2023-03-10 12:52:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shorturl')
-const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
